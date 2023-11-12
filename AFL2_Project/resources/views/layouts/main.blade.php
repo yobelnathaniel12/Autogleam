f24b7253519ca2f4c45ffab3437d3c06964a8dbc
jasonmiracleg <jasonmiracle15@gmail.com>
2023-11-12 12:53:57 +0700

    Adding Icon to Browser

--- AFL2_Project/resources/views/layouts/main.blade.php
+++ AFL2_Project/resources/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" type="x-icon" href="image/autogleam_logo_white_ver.png">
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
