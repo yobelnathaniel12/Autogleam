fc89102d0a39581a8d1fbfad682dc403bd2612f5
jasonmiracleg <jasonmiracle15@gmail.com>
2023-11-12 19:14:33 +0700

    Final Design

--- AFL2_Project/resources/views/page_content/services.blade.php
+++ AFL2_Project/resources/views/page_content/services.blade.php
@@ -18,14 +18,14 @@
                 </div>
             </div> --}}
             <div
-                class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-md md:flex-row md:max-w-xl">
+                <img class="object-cover w-full rounded-t-lg h-auto md:w-48 md:rounded-none md:rounded-l-lg w-auto"
                     src="image/inova-zenix.png" alt="">
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Autohome
+                <div class="flex flex-col justify-between p-4 leading-normal md:border-l-2">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-center">Autohome
                         Service
                     </h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Bingung mengatur waktu untuk cuci mobil?
+                    <p class="mb-3 font-normal">Bingung mengatur waktu untuk cuci mobil?
                         Tenang, Autogleam memiliki layanan Autohome Service yang dapat membuat customer nyaman dengan order
                         cuci mobil di rumah</p>
                     <a href="https://wa.me/6281336183669" class="flex flex-row justify-center">
@@ -37,14 +37,14 @@
                 </div>
             </div>
             <div
-                class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800">
+                class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl">
                 <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
                     src="image/shampoo_autogleam.jpg" alt="">
                 <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Autogleam's
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-center">Autogleam's
                         Products
                     </h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Bingung cari market untuk kebutuhan
+                    <p class="mb-3 font-normal">Bingung cari market untuk kebutuhan
                         otomotif? Autogleam adalah solusinya karena kami menyediakan berbagai produk yang dibutuhkan
                         para customer untuk
                         merawat
